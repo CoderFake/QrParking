d676d1b688d687f4571e1ffe78e728251345ea99
coder fake <hoangdieu22022002@gmail.com>
2024-10-20 16:42:27 +0700

    update database config in settings.py

--- smartparking/smartparking/settings.py
+++ smartparking/smartparking/settings.py
@@ -114,10 +114,10 @@ DATABASES = {
         "NAME": os.environ.get("DB_NAME"),
         "USER": os.environ.get("DB_USER"),
         "PASSWORD": os.environ.get("DB_PASSWORD"),
-        # "HOST": os.environ.get("DB_HOST"),
-        # "PORT": os.environ.get("DB_PORT"),
-        "HOST": "localhost",
-        "PORT": 15300,
+        "HOST": os.environ.get("DB_HOST"),
+        "PORT": os.environ.get("DB_PORT"),
+        # "HOST": "localhost",
+        # "PORT": 15300,
     }
 }
 
