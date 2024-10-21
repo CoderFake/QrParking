51ec4a507a3da17b34dbbc3adabe139eab924091
coder fake <hoangdieu22022002@gmail.com>
2024-10-21 15:54:27 +0700

    create payment urls

--- smartparking/smartparking/urls.py
+++ smartparking/smartparking/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("webapp.urls")),
     path("account/", include("account.urls")),
+    path("payment/", include("payment.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
