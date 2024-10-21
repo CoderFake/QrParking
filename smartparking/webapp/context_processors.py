5a55f159d657605d550c7ffb6d708cffcf8ddcde
coder fake <hoangdieu22022002@gmail.com>
2024-10-21 17:04:55 +0700

    update context_processors.py

--- smartparking/webapp/context_processors.py
+++ smartparking/webapp/context_processors.py
@@ -2,14 +2,9 @@ from django.conf import settings
 
 
 def base_url(request):
-    if settings.ENVIRONMENT == 'dev':
-        return {
-            'base_url': f"{settings.MINIO_ENDPOINT_PUBLIC}/{settings.BUCKET_NAME}/"
-        }
-    else:
-        return {
-            'base_url':  f"https://{settings.BUCKET_NAME}.s3.amazonaws.com/"
-        }
+    return {
+        'base_url': f"https://{settings.BUCKET_NAME}.s3.amazonaws.com/"
+    }
 
 
 def balance(request):
