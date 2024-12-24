ff43ea56b66ae7b39c392460baa559bbd90960fc
coder fake <hoangdieu22022002@gmail.com>
2024-12-25 02:43:57 +0700

    update: custom admin urls

--- smartparking/smartparking/urls.py
+++ smartparking/smartparking/urls.py
@@ -1,11 +1,10 @@
-from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-
+from adminapp.admin import admin_site
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin_site.urls),
     path("", include("webapp.urls")),
     path("account/", include("account.urls")),
     path("payment/", include("payment.urls")),
