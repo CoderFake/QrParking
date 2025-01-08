d5dfb963ace2235a561cca462ce77d7353a672cc
coderfake <hoangdieu22022002@gmail.com>
2025-01-08 23:29:53 +0700

    create: add payment admin

--- smartparking/payment/admin.py
+++ smartparking/payment/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Transaction
+from adminapp.admin import admin_site
 
-# Register your models here.
+
+class AdminTransaction(admin.ModelAdmin):
+
+    list_display = ('transaction_code', 'user', 'method', 'amount', 'status', 'created_at')
+    readonly_fields = ('transaction_code', 'user', 'order', 'type', 'method', 'amount', 'created_at', 'status')
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
+admin_site.register(Transaction, AdminTransaction)
