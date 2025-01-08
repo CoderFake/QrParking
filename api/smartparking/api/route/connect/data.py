f5017d83a21145c8bad5400f988b7ab5100a4ef9
coderfake <hoangdieu22022002@gmail.com>
2025-01-08 23:32:49 +0700

    create: add routes for pi get data

--- api/smartparking/api/route/connect/data.py
+++ api/smartparking/api/route/connect/data.py
@@ -2,6 +2,7 @@ import smartparking.service.data as ps
 from smartparking.api.commons import (
     APIRouter,
     Authorized,
+    otp_auth_instance,
     Depends,
     Errors,
     Query,
@@ -18,7 +19,49 @@ from smartparking.api.shared.schema import SelfContainedGenerateSchema
 
 router = APIRouter()
 
-#
+@router.get(
+    "/parking-infor/{parking_id}",
+    status_code=200,
+    responses={
+        200: {"description": "Parking information."},
+        404: {"description": "Parking not found."},
+    },
+)
+async def get_parking_infor(
+    parking_id: str,
+    _: None =  Depends(otp_auth_instance.auth_with_otp),
+) -> vr.ParkingInfor:
+
+    parking = await ps.get_parking_infor(str(parking_id))
+
+    if not parking:
+        abort_with(status=404, message="Parking not found.")
+
+    parking_infor = vr.ParkingInfor.of(parking.value)
+    return parking_infor
+
+
+
+
+@router.get(
+    "/user-data",
+    status_code=200,
+    responses={
+        200: {"description": "User data."},
+        404: {"description": "User not found."},
+    },
+)
+async def get_user_data(
+    _: None = Depends(otp_auth_instance.auth_with_otp),
+) -> vr.UserData:
+
+    user_data = await ps.get_user_data()
+    if not user_data:
+        abort_with(status=404, message="User not found.")
+
+    return vr.UserData.of(user_data.value)
+
+
 # @router.post(
 #     "/images",
 #     status_code=201,
