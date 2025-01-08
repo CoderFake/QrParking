66262924331b690e1a001498102c436798e0042f
coderfake <hoangdieu22022002@gmail.com>
2025-01-08 23:33:11 +0700

    create: add services for pi get data

--- api/smartparking/service/data.py
+++ api/smartparking/service/data.py
@@ -1,26 +1,48 @@
-import json
-from typing import Any, Dict
-from uuid import uuid4
+from typing import List, Tuple
+from sqlalchemy.orm import joinedload, selectinload
 
-from smartparking.ext.image.base import ImageContent, load_image
-from pydantic import RootModel
-from sqlalchemy import and_
-from sqlalchemy.exc import SQLAlchemyError
-from sqlalchemy.sql import desc, func
+from .commons import *
 
-from .commons import (
-    Errorneous,
-    Errors,
-    Maybe,
-    Optional,
-    c,
-    datetime,
-    delete,
-    insert,
-    m,
-    r,
-    select,
-    service,
-    update,
-)
+@service
+async def get_parking_infor(parking_id: str) -> Optional[c.ParkingInfo]:
+
+    result = await r.tx.execute(
+        select(m.ParkingSettings)
+        .options(
+            joinedload(m.ParkingSettings.month_tickets).raiseload('*'),
+            joinedload(m.ParkingSettings.vehicles).raiseload('*'),
+            joinedload(m.ParkingSettings.timers).raiseload('*'),
+            joinedload(m.ParkingSettings.cameras).raiseload('*'),
+        )
+        .filter(m.ParkingSettings.id == parking_id)
+    )
+
+    parking = result.scalar()
+
+    if not parking:
+        return None
+
+    return c.ParkingInfo.from_orm(parking)
+
+
+@service
+async def get_user_data() -> List[Tuple[c.Me, List[c.UserVehicle], List[c.UserTicket]]]:
+    result = await r.tx.execute(
+        select(m.Account)
+        .options(
+            selectinload(m.Account.vehicles).selectinload(m.VehicleInfor.tickets),
+            selectinload(m.Account.tickets)
+        )
+    )
+
+    accounts_with_details = []
+    for account in result.scalars():
+        vehicles = account.vehicles
+        tickets = [ticket for vehicle in vehicles for ticket in vehicle.tickets]
+        accounts_with_details.append((account, vehicles, tickets))
+
+    return accounts_with_details
+
+
+
 
